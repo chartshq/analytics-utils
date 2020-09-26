6a295372585f70ebf4699a2ff789167a9c517193
Rousan Ali <rousanali786@gmail.com>
2020-09-26 13:33:47 +0530

    Update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-export { default as analyticsPluginDebugger } from './src/analytics-plugin-debugger';
-export { default as analyticsPluginPostHog } from './src/analytics-plugin-posthog';
-export { default as extendAnalytics } from './src/extend-analytics';
+import analyticsPluginDebugger from './src/analytics-plugin-debugger';
+import analyticsPluginPostHog from './src/analytics-plugin-posthog';
+import extendAnalytics from './src/extend-analytics';
+
+export { analyticsPluginDebugger, analyticsPluginPostHog, extendAnalytics };
